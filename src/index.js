4795e0ba97cc9ae8f61beaac60c1748444ac5c83
arunpandiancse2003@gmail.com <arunpandiancse2003@gmail.com>
2024-07-17 13:59:35 +0530

    fdh

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function fn()
+{
+    if(Long.uname.value=="admin" && Long.m.value=="admin")
+{
+    alert("Login Successful: You are authorised");
+}
+else
+{
+    alert("Invalid Security Details");
+}
+}
